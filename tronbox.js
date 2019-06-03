4a645a45c70e7284adff5b07e7d6005dcb0da58c
Srinjoy Chakravarty <chakravarty.s@husky.neu.edu>
2019-06-04 05:03:47 +0530

    put ssl in footer

--- tronbox.js
+++ tronbox.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     mainnet: {
-      privateKey: 'bf101c5933e07d6a8008f58ff187fb109eca262b30ce701f69c28199acfb6cc6',
+      privateKey: '',
       consume_user_resource_percent: 30,
       fee_limit: 100000000,
       fullNode: "https://api.trongrid.io",
